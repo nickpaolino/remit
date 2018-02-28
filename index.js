7da4ba5a0331a91684ecc7cf127b8819c63710b8
Nick Paolino <github email address>
2018-02-28 11:23:41 -0500

    Added passport js and google strategy for google oauth

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 
+// creates the express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ test: "heroku" });
-});
+const keys = require("./config/keys");
+
+// imports passport's built-in google oauth strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+  })
+);
 
 // uses Heroku's port if it exists, otherwise 5000
 const PORT = process.env.PORT || 5000;
