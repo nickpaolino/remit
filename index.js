cbb8ecaea4069b49fbac8820fb4cd32d7650fe76
Nick Paolino <github email address>
2018-02-28 12:44:50 -0500

    Tested google oauth flow

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const app = express();
 
 const keys = require("./config/keys");
 
+const passport = require("passport");
+
 // imports passport's built-in google oauth strategy
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token:", accessToken);
+      console.log("refresh token:", refreshToken);
+      console.log("profile:", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // uses Heroku's port if it exists, otherwise 5000
 const PORT = process.env.PORT || 5000;
