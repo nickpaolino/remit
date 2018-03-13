be9af09c203f4074a1c6a1ce848bbfc37304e8df
Nick Paolino <github email address>
2018-03-13 12:50:41 -0400

    Added axios and redux-thunk libraries

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Login with Google</a>
+      </div>
+    );
+  }
+}
+
+export default Header;
